77fb5330aed6fed77bfe53e61d23334dfb4a1c99
Elessandro Prestes Macedo <elessandropm@gmail.com>
2018-11-27 17:55:08 -0200

    Adicionado botao de voltar ao topo

--- _js/main.js
+++ _js/main.js
@@ -1,12 +1,35 @@
-   $(function() {
-                $("div#menu-btn").click(function() {
-                    $("nav ul#menu-mobile").toggle();
-                });
-                
-                $(window).resize(function() {
-                   var largura = $(window).width(); 
-                    if ( largura >= 701 ) {
-                        $("nav ul#menu-mobile").hide();   
-                    }
-                });
-            });
+  
+$(document).ready(function () {
+    $('.jq-back-to-top').hide();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.jq-back-to-top').fadeIn();
+        } else {
+            $('.jq-back-to-top').fadeOut();
+        }
+    });
+
+    $('.jq-back-to-top').click(function () {
+        $('html,main,body,header').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+});
+
+
+
+  
+$(function () {
+    $("div#menu-btn").click(function () {
+        $("nav ul#menu-mobile").toggle();
+    });
+
+    $(window).resize(function () {
+        var largura = $(window).width();
+        if (largura >= 701) {
+            $("nav ul#menu-mobile").hide();
+        }
+    });
+});
