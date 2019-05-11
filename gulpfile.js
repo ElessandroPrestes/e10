135702000ed61d55f87450fdd10c733a507d7dff
Elessandro Prestes Macedo <elessandropm@gmail.com>
2019-05-11 11:53:45 -0300

    Alterado algumas pastas do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
 let gulp = require('gulp');
 let watch = require('gulp-watch');
-let uglify = require('gulp-uglify');
-let pipeline = require('readable-stream').pipeline;
 let pump  =  require('pump');
 let concat = require('gulp-concat');
 let notify = require('gulp-notify');
 let css = require('gulp-clean-css');
+let rename = require("gulp-rename");
+let uglify = require('gulp-uglify-es').default;
 
-/*Minificando css*/
+
 gulp.task('mincss', () =>{
   return gulp.src('assets/css/*css')
   .pipe(css())
   .on("error", notify.onError("Error: <%= error.message %>"))
-  .pipe(gulp.dest('dist/css/minify/'));
+  .pipe(gulp.dest('dist/css/'));
 });
 
 
-/*Minificando JS */
+gulp.task('minjs', () =>{
+  return gulp.src('js/index.js')
+  .pipe(rename('index.min.js'))
+  .pipe(uglify())
+  .on("error", notify.onError("Error: <%= error.message %>"))
+  .pipe(gulp.dest('dist/js/'))
+});
 
-
-//Deixando o function automatizado.
-gulp.task('default',['mincss'], () => {
+gulp.task('default',['mincss,minjs'], () => {
     gulp.watch('assets/css/*css', ['mincss']);
+    gulp.watch('js/index.js', ['minjs']);
 });
--- js/index.js
+++ js/index.js
@@ -1,13 +1,36 @@
 
-(function(win, doc){
+((win, doc) =>{
 'use strict';
+
+    let modal = doc.querySelector('[data-js="jsModal"]');
+
+    let btn = doc.querySelector('[data-js="jsBtn"]');
+
+    let close = doc.querySelector('[data-js="jsClose"]');
+
+    btn.addEventListener("click", (e) =>{
+        e.preventDefault();
+        modal.style.display = "block";
+    });
+
+    close.addEventListener("click", (e) =>{
+        e.preventDefault();
+        modal.style.display = "none";
+    });
+
+    win.addEventListener("click", (e) =>{
+        if (e.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+
 
     const target = doc.querySelectorAll('[data-anime]');
     const animationClass = 'animate';
 
-    function animeScroll(){
+      function animeScroll(){
         const windowTop = win.pageYOffset + ((win.innerHeight * 0.75));
-        target.forEach(function(element){
+        target.forEach((element) =>{
             if((windowTop) > element.offsetTop){
                 element.classList.add(animationClass);
             }else{
@@ -18,46 +41,6 @@
 
     animeScroll();
 
-    
-    /*Api github
-        function getApiGitHub(){
-        let ajax = new XMLHttpRequest();
-        let data = 'https://api.github.com/users/elessandroprestes';
-        ajax.open('GET', data, true);
-        ajax.send();
-
-        ajax.onreadystatechange = function () {
-            if (ajax.readyState == 4 && ajax.status == 200){
-                data.foreach((element)=>{
-                    let $div = doc.querySelector('[data-js="dropdown-js"]');
-                });
-            }
-        };
-        }
-    */
-//Modal
-    
-    let modal = doc.querySelector('[data-js="jsModal"]');
-
-    let btn = doc.querySelector('[data-js="jsBtn"]');
-
-    let close = doc.querySelector('[data-js="jsClose"]');
-
-    btn.addEventListener("click", function(e){
-        e.preventDefault();
-        modal.style.display = "block";
-    });
-
-    close.addEventListener("click", function(e) {
-        e.preventDefault();
-        modal.style.display = "none";
-    });
-
-    win.addEventListener("click", function(e) {
-        if (e.target == modal) {
-            modal.style.display = "none";
-        }
-    });
 
 
 })(window, document);
