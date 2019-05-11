cc9e13d83edd987c538f5f36046783eb33c16ef0
Elessandro Prestes Macedo <elessandropm@gmail.com>
2019-05-11 14:47:55 -0300

    Minificado o arquivo js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,12 @@
-let gulp = require('gulp');
-let watch = require('gulp-watch');
-let pump  =  require('pump');
-let concat = require('gulp-concat');
-let notify = require('gulp-notify');
-let css = require('gulp-clean-css');
-let rename = require("gulp-rename");
-let uglify = require('gulp-uglify-es').default;
 
+import  gulp from 'gulp';
+import watch from 'gulp-watch';
+import concat from'gulp-concat';
+import notify from'gulp-notify';
+import css from'gulp-clean-css';
+import uglify from 'gulp-uglify-es';
+
+import rename from 'gulp-rename';
 
 gulp.task('mincss', () =>{
   return gulp.src('assets/css/*css')
@@ -20,7 +20,7 @@ gulp.task('minjs', () =>{
   return gulp.src('js/index.js')
   .pipe(rename('index.min.js'))
   .pipe(uglify())
-  .on("error", notify.onError("Error: <%= error.message %>"))
+  //.on("error", notify.onError("Error: <%= error.message %>"))
   .pipe(gulp.dest('dist/js/'))
 });
 
