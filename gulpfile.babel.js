413bfeaaa058d27c360165e0de91def2007f0270
Elessandro Prestes Macedo <elessandrodev@gmail.com>
2019-05-29 17:23:09 -0300

    Altera arquivo gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,15 +7,15 @@ import css from'gulp-clean-css';
 import uglify from 'gulp-uglify-es';
 import rename from 'gulp-rename';
 
-gulp.task('concatcss', () =>{
+gulp.task('concss', () =>{
     return gulp.src('assets/css/*.css')
-    .pipe(concat("style.css"))
+    .pipe(concat("style.con.css"))
     .on("error", notify.onError("Error: <%= error.message %>"))
     .pipe(gulp.dest('dist/css/concat/'));
 });
 
 gulp.task('mincss', () =>{
-  return gulp.src('dist/css/concat/style.css')
+  return gulp.src('dist/css/concat/style.con.css')
   .pipe(rename('style.min.css'))
   .pipe(css())
   .on("error", notify.onError("Error: <%= error.message %>"))
@@ -31,8 +31,8 @@ gulp.task('minjs', () =>{
   .pipe(gulp.dest('dist/js/'))
 });
 
-gulp.task('default',['concatcss','mincss','minjs'], () => {
-    gulp.watch('assets/css/*css', ['concatcss']);
+gulp.task('default',['concss','mincss','minjs'], () => {
+    gulp.watch('assets/css/*css', ['concss']);
     gulp.watch('dist/css/concat/style.css', ['mincss']);
     gulp.watch('js/index.js', ['minjs']);
 });
