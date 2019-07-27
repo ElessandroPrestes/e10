1e5e00ec92d4a1dda9c7ef53c029a58ac9fdb7f2
Elessandro Prestes Macedo <elessandrodev@gmail.com>
2019-07-27 18:05:36 -0300

    Altera mobile first design do projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,42 @@
+((win, doc) => {
+  ' use strict ';
 
-((win, doc) =>{
-'use strict';
+  const modal = doc.querySelector('[data-js="jsModal"]');
 
-    let modal = doc.querySelector('[data-js="jsModal"]');
+  const btn = doc.querySelector('[data-js="jsBtn"]');
 
-    let btn = doc.querySelector('[data-js="jsBtn"]');
+  const close = doc.querySelector('[data-js="jsClose"]');
 
-    let close = doc.querySelector('[data-js="jsClose"]');
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.style.display = 'block';
+  });
 
-    btn.addEventListener("click", (e) =>{
-        e.preventDefault();
-        modal.style.display = "block";
-    });
+  close.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.style.display = 'none';
+  });
 
-    close.addEventListener("click", (e) =>{
-        e.preventDefault();
-        modal.style.display = "none";
-    });
-
-    win.addEventListener("click", (e) =>{
-        if (e.target == modal) {
-            modal.style.display = "none";
-        }
-    });
-
-
-    const target = doc.querySelectorAll('[data-anime]');
-    const animationClass = 'animate';
-
-      function animeScroll(){
-        const windowTop = win.pageYOffset + ((win.innerHeight * 0.75));
-        target.forEach((element) =>{
-            if((windowTop) > element.offsetTop){
-                element.classList.add(animationClass);
-            }else{
-                element.classList.remove(animationClass);
-            }
-        })
+  win.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
     }
-
-    animeScroll();
-
+  });
 
 
+  const target = doc.querySelectorAll('[data-anime]');
+  const animationClass = 'animate';
+
+  function animeScroll() {
+    const windowTop = win.pageYOffset + ((win.innerHeight * 0.75));
+    target.forEach((element) => {
+      if ((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    });
+  }
+
+  animeScroll();
 })(window, document);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,38 +1,35 @@
-
-import  gulp from 'gulp';
-import watch from 'gulp-watch';
-import concat from'gulp-concat-css';
-import notify from'gulp-notify';
-import css from'gulp-clean-css';
+import gulp from 'gulp';
+import concat from 'gulp-concat-css';
+import notify from 'gulp-notify';
+import css from 'gulp-clean-css';
 import uglify from 'gulp-uglify-es';
 import rename from 'gulp-rename';
 
-gulp.task('concss', () =>{
-    return gulp.src('assets/css/*.css')
-    .pipe(concat("style.con.css"))
-    .on("error", notify.onError("Error: <%= error.message %>"))
+gulp.task('concss', () => {
+  gulp.src('assets/css/*.css')
+    .pipe(concat('style.con.css'))
+    .on('error', notify.onError('Error: <%= error.message %>'))
     .pipe(gulp.dest('dist/css/concat/'));
 });
 
-gulp.task('mincss', () =>{
-  return gulp.src('dist/css/concat/style.con.css')
-  .pipe(rename('style.min.css'))
-  .pipe(css())
-  .on("error", notify.onError("Error: <%= error.message %>"))
-  .pipe(gulp.dest('dist/css/minify/'));
+gulp.task('mincss', () => {
+  gulp.src('dist/css/concat/style.con.css')
+    .pipe(rename('style.min.css'))
+    .pipe(css())
+    .on('error', notify.onError('Error: <%= error.message %>'))
+    .pipe(gulp.dest('dist/css/minify/'));
 });
 
-
-gulp.task('minjs', () =>{
-  return gulp.src('assets/js/index.js')
-  .pipe(rename('index.min.js'))
-  .pipe(uglify())
-  .on("error", notify.onError("Error: <%= error.message %>"))
-  .pipe(gulp.dest('dist/js/'))
+gulp.task('minjs', () => {
+  gulp.src('assets/js/index.js')
+    .pipe(rename('index.min.js'))
+    .pipe(uglify())
+    .on('error', notify.onError('Error: <%= error.message %>'))
+    .pipe(gulp.dest('dist/js/'));
 });
 
-gulp.task('default',['concss','mincss','minjs'], () => {
-    gulp.watch('assets/css/*css', ['concss']);
-    gulp.watch('dist/css/concat/style.css', ['mincss']);
-    gulp.watch('js/index.js', ['minjs']);
+gulp.task('default', ['concss', 'mincss', 'minjs'], () => {
+  gulp.watch('assets/css/*css', ['concss']);
+  gulp.watch('dist/css/concat/style.con.css', ['mincss']);
+  gulp.watch('assets/js/index.js', ['minjs']);
 });
