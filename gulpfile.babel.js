9c2632364db54d2d2756591b4e8b4b0d194bd2bf
Elessandro Prestes Macedo <elessandrodev@gmail.com>
2019-07-29 11:19:41 -0300

    Altera task do gulp referente a concatenacao de arquivo .css e adiciona task para img.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,8 @@
 import gulp from 'gulp';
-import concat from 'gulp-concat-css';
+import concat from 'gulp-concat';
 import notify from 'gulp-notify';
 import css from 'gulp-clean-css';
+import imagemin from 'gulp-imagemin';
 import uglify from 'gulp-uglify-es';
 import rename from 'gulp-rename';
 
@@ -9,15 +10,15 @@ gulp.task('concss', () => {
   gulp.src('assets/css/*.css')
     .pipe(concat('style.con.css'))
     .on('error', notify.onError('Error: <%= error.message %>'))
-    .pipe(gulp.dest('dist/css/concat/'));
+    .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('mincss', () => {
-  gulp.src('dist/css/concat/style.con.css')
+  gulp.src('dist/css/style.con.css')
     .pipe(rename('style.min.css'))
     .pipe(css())
     .on('error', notify.onError('Error: <%= error.message %>'))
-    .pipe(gulp.dest('dist/css/minify/'));
+    .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('minjs', () => {
@@ -25,11 +26,17 @@ gulp.task('minjs', () => {
     .pipe(rename('index.min.js'))
     .pipe(uglify())
     .on('error', notify.onError('Error: <%= error.message %>'))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('assets/js/'));
+});
+
+gulp.task('imagemin', () => {
+  gulp.src('assets/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('default', ['concss', 'mincss', 'minjs'], () => {
-  gulp.watch('assets/css/*css', ['concss']);
-  gulp.watch('dist/css/concat/style.con.css', ['mincss']);
+  gulp.watch('assets/css/*.css', ['concss']);
+  gulp.watch('assets/css/style.con.css', ['mincss']);
   gulp.watch('assets/js/index.js', ['minjs']);
 });
